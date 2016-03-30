883004471d72b4f6e9005c8d65c40ce2c3c5153b
dingyunxiang <1210955086@qq.com>
2016-03-30 20:10:31 +0800

    添加json jar包，提供JsonAction支持，login（）返回值

---
+++ GraduationDesignSystem/src/edu/nefu/gdms/action/JsonAction.java
@@ -0,0 +1,64 @@
+package edu.nefu.gdms.action;
+
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import edu.nefu.gdms.domain.Student;
+import edu.nefu.gdms.service.StudentManager;
+
+public class JsonAction extends ActionSupport  {
+
+	/**
+	 * 
+	 */
+	private String username;
+	private String password;
+	private StudentManager studentManager;
+	private String loginRs;
+	
+	public String getLoginRs() {
+		return loginRs;
+	}
+	public void setLoginRs(String loginRs) {
+		this.loginRs = loginRs;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public StudentManager getStudentManager() {
+		return studentManager;
+	}
+	public void setStudentManager(StudentManager studentManager) {
+		this.studentManager = studentManager;
+	}
+
+	public String login() {
+		if (studentManager.login(username, password)) {
+			loginRs = "success";
+			return SUCCESS;
+		}
+		else {
+			loginRs = "fail";
+			return ERROR;
+		}
+			
+	}
+	
+	public String getAll(){
+		List<Student> studentList = studentManager.getAll();
+		return "studentList";
+	}
+	
+}
