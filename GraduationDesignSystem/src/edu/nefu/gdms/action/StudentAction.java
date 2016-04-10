9dffa326846f6c3e4439fd47d6950ec06de27e24
dingyunxiang <1210955086@qq.com>
2016-04-10 13:35:56 +0800

    调整Action代码风格

--- GraduationDesignSystem/src/edu/nefu/gdms/action/StudentAction.java
+++ GraduationDesignSystem/src/edu/nefu/gdms/action/StudentAction.java
@@ -11,142 +11,144 @@ import com.opensymphony.xwork2.ActionSupport;
 import edu.nefu.gdms.beans.StudentBean;
 import edu.nefu.gdms.service.StudentManager;
 
-public class StudentAction extends ActionSupport{
+public class StudentAction extends ActionSupport {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	Gson gson=new Gson();
+	Gson gson = new Gson();
 	HttpServletRequest request;
 	private String number;
 	private String pwd;
 	private StudentBean studentBean;
 	private StudentManager studentManager;
-	//登录结果
-	private String login;  
-	//查询所有学生
+	// 登录结果
+	private String login;
+	// 查询所有学生
 	private String getAll;
-	//移除
+	// 移除
 	private String remove;
-	//修改
+	// 修改
 	private String modify;
-	//保存
+	// 保存
 	private String save;
-	
-	
-	
-	public String getNumber() {
-		return number;
-	}
-	public void setNumber(String number) {
-		this.number = number;
-	}
-	public String getPwd() {
-		return pwd;
-	}
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
+
+	// 跳转部分
+	// 跳转到主页面
+	public String index() {
+		return "index";
 	}
 
-
-
-	
-	 
-	 
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	public String getGetAll() {
-		return getAll;
-	}
-	public void setGetAll(String getAll) {
-		this.getAll = getAll;
-	}
-	public String getRemove() {
-		return remove;
-	}
-	public void setRemove(String remove) {
-		this.remove = remove;
-	}
-	public String getModify() {
-		return modify;
-	}
-	public void setModify(String modify) {
-		this.modify = modify;
-	}
-	public String getSave() {
-		return save;
-	}
-	public void setSave(String save) {
-		this.save = save;
-	}
-	public StudentManager getStudentManager() {
-		return studentManager;
-	}
-	public void setStudentManager(StudentManager studentManager) {
-		this.studentManager = studentManager;
-	}
-	public StudentBean getStudentBean() {
-		return studentBean;
-	}
-	public void setStudentBean(StudentBean studentBean) {
-		this.studentBean = studentBean;
-	}
-	
-	
-	
-
-	//判断登录
+	// 判断登录
 	public String login() {
 		login = "";
-		//System.out.println(studentBean.getNumber()+".."+studentBean.getPwd());
-		try{
-			if (studentManager.login(number,pwd)) {
+		// System.out.println(studentBean.getNumber()+".."+studentBean.getPwd());
+		try {
+			if (studentManager.login(number, pwd)) {
 				login = "success";
 				studentBean = studentManager.getByStuNumber(number);
-				 ActionContext.getContext().getSession().put("student", studentBean);
-			}
-			else{
+				ActionContext.getContext().getSession().put("student", studentBean);
+			} else {
 				login = "fail";
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			login = "fail";
 			e.printStackTrace();
 		}
 		return SUCCESS;
-		
+
 	}
-	//退出
-	public String out(){
+
+	// 退出
+	public String out() {
 		ActionContext.getContext().getSession().put("student", null);
 		return "out";
 	}
-	
+
 	public String modify() {
 		try {
 			modify = "success";
 			studentManager.modify(studentBean);
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			modify = "fail";
 			e.printStackTrace();
-			
+
 		}
 		return SUCCESS;
 	}
-	
-	
-	
-	
-	
-	//跳转到主页面
-	public String index(){
-		return "index";
+
+	public String getNumber() {
+		return number;
 	}
-	
-	
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getGetAll() {
+		return getAll;
+	}
+
+	public void setGetAll(String getAll) {
+		this.getAll = getAll;
+	}
+
+	public String getRemove() {
+		return remove;
+	}
+
+	public void setRemove(String remove) {
+		this.remove = remove;
+	}
+
+	public String getModify() {
+		return modify;
+	}
+
+	public void setModify(String modify) {
+		this.modify = modify;
+	}
+
+	public String getSave() {
+		return save;
+	}
+
+	public void setSave(String save) {
+		this.save = save;
+	}
+
+	public StudentManager getStudentManager() {
+		return studentManager;
+	}
+
+	public void setStudentManager(StudentManager studentManager) {
+		this.studentManager = studentManager;
+	}
+
+	public StudentBean getStudentBean() {
+		return studentBean;
+	}
+
+	public void setStudentBean(StudentBean studentBean) {
+		this.studentBean = studentBean;
+	}
+
 }
--- GraduationDesignSystem/src/edu/nefu/gdms/action/TeacherAction.java
+++ GraduationDesignSystem/src/edu/nefu/gdms/action/TeacherAction.java
@@ -15,75 +15,118 @@ public class TeacherAction extends ActionSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private TeacherManager teacherManager;
-	
-	//从前台获取的文件
+
+	// 从前台获取的文件
 	private String number;
 	private String pwd;
 	private TeacherBean teacherBean;
-	private String login; 
+	private String login;
 	private TitleBean titleBean;
-	
-	
-	//上传的文件，文件的名字
+
+	// 上传的文件，文件的名字
 	private File file;
 	private String filename;
-	
-	//需要传回前台的文件
+
+	// 需要传回前台的文件
 	private String addTitle;
-	
-	
-	
-	
+
+	// 登陆
+	public String login() {
+		login = "";
+		try {
+			if (teacherManager.login(number, pwd)) {
+				login = "success";
+				teacherBean = teacherManager.getByTeaNumber(number);
+				ActionContext.getContext().getSession().put("teacher", teacherBean);
+			} else {
+				login = "fail";
+			}
+
+		} catch (Exception e) {
+			login = "fail";
+			e.printStackTrace();
+		}
+		return SUCCESS;
+	}
+
+	// 跳转到主页面
+	public String index() {
+		return "index";
+	}
+
+	// 退出
+	public String out() {
+		ActionContext.getContext().getSession().put("student", null);
+		return "out";
+	}
+
+	public String addTitle(TitleBean titleBean) {
+		addTitle = teacherManager.addTitle(titleBean, file, filename);
+		return SUCCESS;
+	}
+
 	public String getAddTitle() {
 		return addTitle;
 	}
+
 	public void setAddTitle(String addTitle) {
 		this.addTitle = addTitle;
 	}
+
 	public TitleBean getTitleBean() {
 		return titleBean;
 	}
+
 	public void setTitleBean(TitleBean titleBean) {
 		this.titleBean = titleBean;
 	}
+
 	public String getNumber() {
 		return number;
 	}
+
 	public void setNumber(String number) {
 		this.number = number;
 	}
+
 	public String getPwd() {
 		return pwd;
 	}
+
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
-	
-	
 
 	public File getFile() {
 		return file;
 	}
+
 	public void setFile(File file) {
 		this.file = file;
 	}
+
 	public String getFilename() {
 		return filename;
 	}
+
 	public void setFilename(String filename) {
 		this.filename = filename;
 	}
+
 	public TeacherBean getTeacherBean() {
 		return teacherBean;
 	}
+
 	public void setTeacherBean(TeacherBean teacherBean) {
 		this.teacherBean = teacherBean;
 	}
+
 	public String getLogin() {
 		return login;
 	}
+
 	public void setLogin(String login) {
 		this.login = login;
 	}
@@ -91,42 +134,8 @@ public class TeacherAction extends ActionSupport {
 	public TeacherManager getTeacherManager() {
 		return teacherManager;
 	}
+
 	public void setTeacherManager(TeacherManager teacherManager) {
 		this.teacherManager = teacherManager;
-	}
-	//登陆
-	public String login() {
-		login="";
-		try{
-			if (teacherManager.login(number, pwd)) {
-				login = "success";
-				teacherBean = teacherManager.getByTeaNumber(number);
-				 ActionContext.getContext().getSession().put("teacher", teacherBean);
-			}
-			else{
-				login = "fail";
-			}
-			
-		}catch(Exception e) {
-			login="fail";
-			e.printStackTrace();	
-		}
-		return SUCCESS;
-	}
-	//跳转到主页面
-	public String index(){
-		return "index";
-	}
-	
-	//退出
-	public String out(){
-		ActionContext.getContext().getSession().put("student", null);
-		return "out";
-	}
-	
-	
-	public String addTitle(TitleBean titleBean){
-		addTitle = teacherManager.addTitle(titleBean, file, filename);
-		return SUCCESS;
 	}
 }
