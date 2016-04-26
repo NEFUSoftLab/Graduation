65ef8a01a0473f00bdf90046d007e57e1dec41f7
pyl1995825 <604512714@qq.com>
2016-04-26 20:10:15 +0800

    修改教师

--- GraduationDesignSystem/WebContent/static/js/admin/ad_adte.js
+++ GraduationDesignSystem/WebContent/static/js/admin/ad_adte.js
@@ -11,19 +11,29 @@ var Manage = {
 			for(var i in params) {
 				var name = params[i].split("=")[0];
 				var value = params[i].split("=")[1];
-				console.log(value);
-				data.name = value;
+				data[name] = value;
 			}
-			console.log(data.);
-//			$.post("admin-modifyTeacher.action", {data: data}, function() {
-//				console.log('123');
-//			})
+			data = JSON.stringify(data);
+			$.post("admin-modifyTeacher.action", {data: data}, function() {
+				$("#section-container").load('admin-teaForward.action')
+			})
 			return false;
 		});
 	},
 	add: function() {
 		$("#addTeacher").on('click', function() {
-			console.log(decodeURIComponent(Manage.$teaInfo.serialize(), true));
+			console.log('123');
+			var data = {};
+			var params = decodeURIComponent(Manage.$teaInfo.serialize(), true).split("&");
+			for(var i in params) {
+				var name = params[i].split("=")[0];
+				var value = params[i].split("=")[1];
+				data[name] = value;
+			}
+			data = JSON.stringify(data);
+			$.post("admin-addTeacher.action", {data: data}, function() {
+				$("#section-container").load('admin-teaForward.action')
+			})
 		});
 	}
 }
