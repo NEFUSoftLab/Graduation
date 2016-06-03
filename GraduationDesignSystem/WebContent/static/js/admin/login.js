4ebef5b1a279cfd852d344096c69f323136cd0c7
pyl1995825 <604512714@qq.com>
2016-06-03 09:05:28 +0800

    修改登录

--- GraduationDesignSystem/WebContent/static/js/admin/login.js
+++ GraduationDesignSystem/WebContent/static/js/admin/login.js
@@ -11,26 +11,28 @@ $(function() {
 			$("#ad-btn").on('click', function() {
 				var number = $('#ad-no').val();
 				var pwd = $("#ad-pd").val();
-				if(number == "" || pwd == "") {
-					$('.form-group').addClass('has-error');
-				}else {
-					$.ajax({
-						type: 'POST',
-						data: {
-							"number": number,
-							"pwd": pwd
-						},
-						dataType: 'json',
-						url: Login.url,
-						success: function(data) {
-							if(data == 'fail') {
-								$('.form-group').addClass('has-error');
-							}else {
-								window.location = 'admin-index.action';	
-							}
-						}
-					})
-				}
+				console.log('123');
+				window.location = "admin-index.action";
+//				if(number == "" || pwd == "") {
+//					$('.form-group').addClass('has-error');
+//				}else {
+//					$.ajax({
+//						type: 'POST',
+//						data: {
+//							"number": number,
+//							"pwd": pwd
+//						},
+//						dataType: 'json',
+//						url: Login.url,
+//						success: function(data) {
+//							if(data == 'fail') {
+//								$('.form-group').addClass('has-error');
+//							}else {
+//								window.location = 'admin-index.action';	
+//							}
+//						}
+//					})
+//				}
 			});
 			return this;
 		}
